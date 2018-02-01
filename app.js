28e8dc3cf4294efd34600fe9c064de6a37b81144
Jack Evans <j.evans.1310@gmail.com>
2018-02-01 15:56:53 +0000

    building the local file sytem database for the trv units

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express()
 
 app.use(bodyParser.json())
 
+const TrvStorage = require('./lib/trv/trvStorage')
+const trvStorage = new TrvStorage()
+
+app.use((req, res, next) => {
+  req.trvStorage = trvStorage
+  next()
+})
+
 const router = require('./router.js')
 app.use('/api/v1', router)
 
