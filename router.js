81ec62791dd259c745963aca09225525d0fb8d1a
Jack Evans <j.evans.1310@gmail.com>
2018-02-12 18:04:31 +0000

    add GET /trv/:id/temperature API

--- router.js
+++ router.js
@@ -10,6 +10,7 @@ router.get('/trv/:id', trvRequestHandler.getTrvByIDRequestHandler)
 router.put('/trv/:id', trvRequestHandler.updateTrvRequestHandler)
 router.delete('trv/:id', trvRequestHandler.deleteTrvRequestHandler)
 router.get('/trv/:id/isActive', trvRequestHandler.getTrvActivityRequestHandler)
+router.get('/trv/:id/temperature', trvRequestHandler.getTrvTemperatureRequestHandler)
 
 router.get('/test', (req, res) => {
   res.status(200).send({'time': new Date(), 'name': packageJson.name, 'version': packageJson.version})
