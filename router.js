886b3af8b32735763947149388736570ad6bc11f
Jack Evans <j.evans.1310@gmail.com>
2018-02-12 16:09:50 +0000

    add GET /trv/:id/isActive API

--- router.js
+++ router.js
@@ -9,6 +9,7 @@ router.get('/trv', trvRequestHandler.getAllTrvsRequestHandler)
 router.get('/trv/:id', trvRequestHandler.getTrvByIDRequestHandler)
 router.put('/trv/:id', trvRequestHandler.updateTrvRequestHandler)
 router.delete('trv/:id', trvRequestHandler.deleteTrvRequestHandler)
+router.get('/trv/:id/isActive', trvRequestHandler.getTrvActivityRequestHandler)
 
 router.get('/test', (req, res) => {
   res.status(200).send({'time': new Date(), 'name': packageJson.name, 'version': packageJson.version})
