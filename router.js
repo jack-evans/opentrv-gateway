8d058394f76bc48f81ad8d047eb460e174cd3ad5
Jack Evans <j.evans.1310@gmail.com>
2018-04-19 15:45:38 +0100

    add PATCH /trv/:id/activeSchedules, start adding unit testing for the scheduleRequestHandler and the server storage

--- router.js
+++ router.js
@@ -15,6 +15,7 @@ router.get('/trv/:id/isActive', trvRequestHandler.getTrvActivityRequestHandler)
 router.get('/trv/:id/temperature', trvRequestHandler.getTrvTemperatureRequestHandler)
 router.put('/trv/:id/temperature', trvRequestHandler.updateTrvTargetTemperatureRequestHandler)
 router.get('/trv/:id/info', trvRequestHandler.getTrvInfoRequestHandler)
+router.patch('/trv/:id/activeSchedules', trvRequestHandler.patchActiveSchedulesRequestHandler)
 
 // schedule request handlers
 router.post('/schedule', scheduleRequestHandler.createScheduleRequestHandler)
