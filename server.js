3390ee986b2af53b48c7317decd8f4c07a64b6f9
Jack <j.evans.1310@gmail.com>
2018-07-30 22:21:36 +0100

    use cluster to hopefully improve performance of server

--- server.js
+++ server.js
@@ -2,9 +2,27 @@
 
 const app = require('./app')
 const SERVER_PORT = process.env.SERVER_PORT || 3002
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-app.listen(SERVER_PORT, () => {
-  console.log(`server listening on port ${SERVER_PORT}`)
-})
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-require('./cron')
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  app.listen(SERVER_PORT, () => {
+    console.log(`server listening on port ${SERVER_PORT}`)
+  })
+
+  console.log(`Worker ${process.pid} started`);
+}
+
